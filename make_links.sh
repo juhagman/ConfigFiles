8eda0cae8566493fd142de5cf7e41a1ac173c989
Jussi Hagman <juhagman@gmail.com>
2010-01-14 15:12:45 +0200

    links should be added to home directory, always.

--- make_links.sh
+++ make_links.sh
@@ -1,5 +1,5 @@
 DIR=`pwd`
 
-ln -s $DIR/inputrc ../.inputrc
-ln -s $DIR/vimrc ../.vimrc
-ln -s $DIR/bash_profile ../.bash_profile
+ln -s $DIR/inputrc ~/.inputrc
+ln -s $DIR/vimrc ~/.vimrc
+ln -s $DIR/bash_profile ~/.bash_profile

a088decad241467326f0e1a8c3091dae2be72ae4
Jussi Hagman <juhagman@gmail.com>
2010-01-14 15:58:47 +0200

    Modified the installation script.

--- make_links.sh
+++ make_links.sh
@@ -1,5 +1,38 @@
-DIR=`pwd`
+# vim: set filetype=perl expandtab tabstop=4 shiftwidth=4 autoindent smartindent nu:
 
-ln -s $DIR/inputrc ~/.inputrc
-ln -s $DIR/vimrc ~/.vimrc
-ln -s $DIR/bash_profile ~/.bash_profile
+function create_link {
+	file=$1
+	if [ -e ~/.$file ] ; then
+		echo Link for $file not created, because it already exists.
+	else
+		ln -s `pwd`/$file ~/.$file
+		echo Created ~/.$file 
+	fi
+}
+
+function remove_link {
+	file=$1
+	if [ -h ~/.$file ] ; then
+		rm ~/.$file
+		echo Link \~/.$file removed.
+	fi
+}
+
+cmd=$1
+case "$cmd" in
+  	'install')
+		create_link inputrc
+		create_link vimrc
+		create_link bash_profile
+		;;
+	'clean')
+		remove_link inputrc
+		remove_link vimrc
+		remove_link bash_profile
+		;;
+	*)
+		echo "USAGE: setup install|clean"
+esac
+
+
+
